0e29bf6893ae352688cdd704d39bba43a3e6503c
Aleksey Borsuk <alesha.borsuk@yandex.ru>
2022-04-20 14:31:12 +0300

    1-21 done

---
+++ JS_HW_1.js
@@ -0,0 +1,12 @@
+let item_1 = 5,
+    item_2 = 3;
+    item_3 = item_1 + item_2,
+    item_4 = 'Yolochka',
+    result_1 = item_3+item_4,
+    result_2 = item_3*item_4,
+    console.log (item_1, item_2, item_3, item_4, result_1, result_2);
+    item_5 = item_3,
+    item_6 = 15,
+    item_6_type = typeof item_6;
+    console.log('item_6 == ', item_6, 'item_6_type == ', item_6_type)
+
