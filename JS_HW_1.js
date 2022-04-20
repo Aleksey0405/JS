8a451104c2dfe52a28f32be89101f12efbd07974
Aleksey Borsuk <alesha.borsuk@yandex.ru>
2022-04-20 17:42:53 +0300

    do 1* task

--- JS_HW_1.js
+++ JS_HW_1.js
@@ -17,7 +17,7 @@ let item_1 = 5,
     age_2 = 18,
     age_3 = 60;
     if (age_1 < age_2) {
-    console.log ("You don't have access cause your age is" , age_1 + '.' + ' ' + "It’s less then need.")
+    console.log ("You don't have access cause your age is" , age_1 + '.' + ' ' + "It's less then need.")
     }else if (age_1 >=  age_2 && age_1 < age_3){
         console.log ('Welcome ! ')
     }else if (age_1  > age_3){
@@ -25,3 +25,17 @@ let item_1 = 5,
     }else{
         console.log('Technical work')
     }
+
+// 1*
+let checkAge = function (age_1,age_2,age_3){
+    if (age_1 < age_2) {
+        console.log ("You don't have access cause your age is" , age_1 + '.' + ' ' + "It's less then need.")
+        }else if (age_1 >=  age_2 && age_1 < age_3){
+            console.log ('Welcome ! ')
+        }else if (age_1  > age_3){
+            console.log ('Keep calm and look Culture channel!')
+        }else{
+            console.log('Technical work')
+        }}
+
+checkAge(17,18,61)
